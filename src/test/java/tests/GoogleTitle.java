ec216f19c40fcde7c88dabb3fa20761f2492b3b5
a-shokriyan <45613496+a-shokriyan@users.noreply.github.com>
2018-12-15 11:29:00 -0500

    Update GoogleTitle.java

--- src/test/java/tests/GoogleTitle.java
+++ src/test/java/tests/GoogleTitle.java
@@ -8,14 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class GoogleTitle {
 	
 	@Test
-	public void getGoogleTitle() {
-		System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver");
-		WebDriver driver = new ChromeDriver();
-		
-		driver.get("http://google.com");
-		String actual = driver.getTitle();
-		AssertJUnit.assertEquals(actual, "Google");
-		driver.quit();
-	}
+	public void
 
 }
